35db9d354a99df94f8999b07aee50bea232b5f52
bajwaharbhajan <bajwharb@shernet.sheridancollege.ca>
2022-06-01 11:38:04 -0400

    first version

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -1,17 +1,27 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package cardtrickice1;
+/*
+ * modified by  harbhajan singh bajwa
+ * student number - 991662481
+ */
+
+import static cardtrickice1.Card.SUITS;
+import java.util.Scanner;
 
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
  * step 3: user card is in  the array 'card is found'
  *
- * @author sivagamasrinivasan,May 23rd
+ * @author sivagamasrinivasan
  */
 public class CardTrickICE1 {
+
+  
 
     /**
      * @param args the command line arguments
@@ -19,15 +29,38 @@ public class CardTrickICE1 {
     public static void main(String[] args) 
     {
         Card[] magicHand = new Card[7]; //Array of object
+    
         for( int i=0;i<magicHand.length;i++)
         {
             Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
-            c1.setSuits("hearts");//random method suit 
+            c1.setValue((int)(Math.random()*13)+1); 
+            c1.setSuits(SUITS[(int)(Math.random()*4)]);
+            magicHand[i]=c1;
+           System.out.println(c1.getValue()+" "+c1.getSuits()); 
         }
+        
         //step 2:take input 
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter  from 1 to 13: ");
+        int num = a.nextInt();
+        
+        Scanner b = new Scanner(System.in);
+        System.out.print("Enter any suit: ");
+        String suit = b.nextLine();
+        
+      
         
         //step 3: match with array 
+
+        boolean key =false;
+        for (Card magicHand1 : magicHand) {
+            if (suit.equalsIgnoreCase(magicHand1.getSuits()) && num == magicHand1.getValue()) {
+                System.out.println("Card successfully found");
+                key=true;
+            }
+        }
+if(!key)
+            System.out.println("Card is not found");
     }
     
 }
